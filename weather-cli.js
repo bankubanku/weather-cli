f32be348cb3a00f098c395134b8fe3b3444817f9
bankubanku <wozniczka.jakub@proton.me>
2024-05-01 21:29:19 +0200

    writing to file

--- weather-cli.js
+++ weather-cli.js
@@ -1,3 +1,48 @@
+function setApiKey() {
+    const fs = require('node:fs');
+    const path = require('path');
+    const os = require('os')
+    const readline = require('node:readline');
+
+    const filePath = path.join(os.homedir(), '.weather-cli');
+    let apiKey;
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.question(`Enter your API key (from OpenWeather): `, answer => {
+        apiKey = answer;
+    })
+
+    fs.writeFile(filePath, apiKey, { flag: 'w+' }, err => {
+        if (err) {
+            console.error(err);
+        }     
+    })
+
+    return apiKey;
+}
+
+
+function getApiKey() {
+    const fs = require('node:fs');
+
+    fs.readFile('~/.weather-cli', 'utf8', (err, data) => {
+        if (err.code === 'ENOENT') { 
+            let apiKey = setApiKey();
+            return apiKey;
+        } else if (err) {
+            console.error(err);
+            return;
+        } 
+        return data;
+    })
+
+}
+
+
 function main() {
     const minimist = require('minimist');
 
@@ -7,8 +52,11 @@ function main() {
         }
     });
     
+    const apiKey = getApiKey();
+    
     if (typeof args.l === 'string') {
         console.log(args.l);
+        console.log(apiKey);
     } else {
         console.log("Please define your city (i. e. weather-cli -l Poznan)");
     }
